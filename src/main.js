4ac8737d34f86f5c260484586f6f6f8c4f6355aa
Code25 <chingisenkhbaatar@gmail.com>
2023-11-24 23:38:56 +0100

    feat:mouse click sets start and end point

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const chessboard = document.getElementById("chessboard");
-  const startBtn = document.getElementById("startBtn");
-  const endBtn = document.getElementById("endBtn");
   const clearBtn = document.getElementById("clearBtn");
   const output = document.getElementById("output");
 
@@ -53,19 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  startBtn.addEventListener("click", () => {
-    output.textContent = "";
-    startCell = null;
-    clear();
-  });
-
-  endBtn.addEventListener("click", () => {
-    output.textContent = "";
-    endCell = null;
-  });
-
-
-  function clear (){
+  const clearBoard = () => {
     output.textContent = "";
     const pathCells = document.querySelectorAll(".path");
     pathCells.forEach(cell => cell.classList.remove("path"));
@@ -75,28 +61,70 @@ document.addEventListener("DOMContentLoaded", () => {
     endCell = null;
   };
 
+  const updateHoverStyles = () => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => cell.classList.remove("start-hover", "end-hover"));
+
+    if (startCell !== null) {
+      const startCellElement = document.querySelector(`.cell[data-row="${startCell.row}"][data-col="${startCell.col}"]`);
+      startCellElement.classList.add("start-hover");
+    }
+
+    if (endCell !== null) {
+      const endCellElement = document.querySelector(`.cell[data-row="${endCell.row}"][data-col="${endCell.col}"]`);
+      endCellElement.classList.add("end-hover");
+    }
+  };
+
   clearBtn.addEventListener("click", () => {
-    clear();
+    clearBoard();
+    updateHoverStyles();
   });
 
-  chessboard.addEventListener("click", (event) => {
+  chessboard.addEventListener("mousedown", (event) => {
     const clickedCell = event.target;
-    if (clickedCell.classList.contains("cell")) {
-      const clickedRow = parseInt(clickedCell.dataset.row);
-      const clickedCol = parseInt(clickedCell.dataset.col);
+    const clickedRow = parseInt(clickedCell.dataset.row);
+    const clickedCol = parseInt(clickedCell.dataset.col);
 
-      if (startCell === null) {
+    if (event.button === 0 || event.button === 2) {
+      const isLeftClick = event.button === 0;
+
+      if (isLeftClick) {
+        clearBoard();
         startCell = { row: clickedRow, col: clickedCol };
         output.textContent = `Start position set to (${startCell.row}, ${startCell.col})`;
-      } else if (endCell === null) {
+      } else {
         endCell = { row: clickedRow, col: clickedCol };
         output.textContent = `End position set to (${endCell.row}, ${endCell.col})`;
-        findShortestPath(startCell, endCell);
+        if (startCell !== null) {
+          findShortestPath(startCell, endCell);
+        }
       }
+
+      updateHoverStyles();
     }
   });
 
-   
+  chessboard.addEventListener("mouseover", (event) => {
+    const hoveredCell = event.target;
+    const isStartHover = hoveredCell.classList.contains("start-hover");
+    const isEndHover = hoveredCell.classList.contains("end-hover");
+
+    if (!isStartHover && !isEndHover) {
+      hoveredCell.classList.add("hover");
+    }
+  });
+
+  chessboard.addEventListener("mouseout", (event) => {
+    const hoveredCell = event.target;
+    hoveredCell.classList.remove("hover");
+  });
+
+
+  // Prevent context menu on right-click
+  chessboard.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+  });
 
   // Create the chessboard
   for (let row = 0; row < 8; row++) {
